61ea42a1489dc188265a441460e617049b79cc0a
ihor97 <daniel.waffner@gmail.com>
2023-05-24 21:20:37 +0100

    141. Using Query Parameters - Practice

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,13 +12,23 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
-
+  allowEdit=false
   constructor(private serversService: ServersService,private actRoute:ActivatedRoute) { }
 
   ngOnInit() {
-    this.actRoute.queryParams.subscribe()
+    this.actRoute.queryParams.subscribe(
+      (queryParams:Params)=>{
+        this.allowEdit=queryParams['allowEdit']=='1'
+      }
+    )
     this.actRoute.fragment.subscribe()
-    this.server = this.serversService.getServer(1);
+    const id=+this.actRoute.snapshot.params['id']
+    this.actRoute.params.subscribe(
+      (params:Params)=>{
+        this.server = this.serversService.getServer(params['id']);
+      }
+    )
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
