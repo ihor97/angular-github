2b8dcf7de39a8bb94d4524c75c200b004e8f6919
ihor97 <daniel.waffner@gmail.com>
2023-05-26 14:36:50 +0100

    145. Outsourcing the Route Configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from "@angular/core";
+import { Routes,RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+
+const appRoutes:Routes=[
+    {path:'',component:HomeComponent},
+    {path:'users',component:UsersComponent,children:[
+      // динамічні параметри /:
+      {path:':id/:name',component:UserComponent},
+    ]},
+    // створення дочірніх роутів і треба забирати servers/
+    {path:'servers',component:ServersComponent,children:[
+      {path:':id',component:ServerComponent},
+      {path:':id/edit',component:EditServerComponent},
+      
+    ]},
+    {path:'not-found',component:PageNotFoundComponent},
+    // перенаправлення всіх неправильних шляхів
+    // і це має стояти в кінці
+    {path:'**',redirectTo:'not-found'}
+  ]
+//   не треба в цей модуль добавляти declarations тому що вони вже є в app.module
+@NgModule({
+    imports:[
+    // конфігурація модуля
+        RouterModule.forRoot(appRoutes)
+    ],
+    // вказує що буде доступним ззовні цього модуля
+    exports:[RouterModule]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 // створення шляхів
-const appRoutes:Routes=[
-  {path:'',component:HomeComponent},
-  {path:'users',component:UsersComponent,children:[
-    // динамічні параметри /:
-    {path:':id/:name',component:UserComponent},
-  ]},
-  // створення дочірніх роутів і треба забирати servers/
-  {path:'servers',component:ServersComponent,children:[
-    {path:':id',component:ServerComponent},
-    {path:':id/edit',component:EditServerComponent},
-    
-  ]},
-  {path:'not-found',component:PageNotFoundComponent},
-  // перенаправлення всіх неправильних шляхів
-  // і це має стояти в кінці
-  {path:'**',redirectTo:'not-found'}
-]
+
 
 @NgModule({
   declarations: [
@@ -46,8 +30,7 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     FormsModule,
-    // конфігурація модуля
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
