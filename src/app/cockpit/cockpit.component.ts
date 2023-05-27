6a6a0ac56a71b644c298e266ed22cdc096029f11
ihor97 <daniel.waffner@gmail.com>
2023-05-27 17:06:00 +0100

    75. Getting Access to the Template & DOM with @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,26 +8,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() newServer = new EventEmitter<{ type: string, name: string, content: string }>()
   @Output('bpCreated') newServerBlueprint = new EventEmitter<{ type: string, name: string, content: string }>()
+  // використання ViewChild для отримання доступу до елемента
+  @ViewChild('serverContentInput') serverContentInput:ElementRef
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer(nameInput:HTMLInputElement,contentInput:HTMLInputElement) {
+  onAddServer(nameInput:HTMLInputElement) {
     const server = {
       type: 'server',
       // отримання даних через локальні силки
       name: nameInput.value,
-      content: contentInput.value
+      content: this.serverContentInput.nativeElement.value
     }
     this.newServer.emit(server)
   }
 
-  onAddBlueprint(nameInput:HTMLInputElement,contentInput:HTMLInputElement) {
+  onAddBlueprint(nameInput:HTMLInputElement) {
     const serverBlueprint = {
       type: 'blueprint',
       name: nameInput.value,
-      content: contentInput.value
+      content: this.serverContentInput.nativeElement.value
     }
     this.newServerBlueprint.emit(serverBlueprint)
   }
