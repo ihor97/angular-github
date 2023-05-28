73f09b3d9548049ce6396d8d6663fae2d914e115
ihor97 <daniel.waffner@gmail.com>
2023-05-28 19:36:26 +0100

    6-87. Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+@Output() switchItem=new EventEmitter<Recipe>()
 recipes:Recipe[]=[
-  new Recipe('A test Recipe','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+  new Recipe('A test Recipe1','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
 ,
-  new Recipe('A test Recipe','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+  new Recipe('A test Recipe2','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
 
 ]
 
@@ -18,5 +19,8 @@ recipes:Recipe[]=[
 
   ngOnInit(): void {
   }
-
+  getSelectedItem(item:Recipe){
+    this.switchItem.emit(item)
+    
+  }
 }
