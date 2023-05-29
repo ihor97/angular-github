3006ca9ffebc64ed9a4e21b11aa0eb8ff865bd6d
ihor97 <daniel.waffner@gmail.com>
2023-05-29 21:59:08 +0100

    8-Closing-the-Dropdown-From-Anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,14 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector:'[appDropdown]'
 })
 export class DropdownDirective{
-    @HostBinding('class.open') addOpen=false
-
-    @HostListener('click') mouseClick(){
-        this.addOpen=!this.addOpen
+    // це модифікація для того щоб одним кліком закривати інші дропдавни
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+      this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+    constructor(private elRef: ElementRef) {}
 
 }
