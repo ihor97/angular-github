516fb34b7a12da705f9a0b62fb71601e7f90802a
ihor97 <daniel.waffner@gmail.com>
2023-05-30 21:24:44 +0100

    10-122. Adding Ingredients to Recipes

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,14 +1,18 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipesService {
     // краще зробити private
 
    getSelectedRecipe=new EventEmitter<Recipe>()  
    private recipes:Recipe[]=[
-        new Recipe('A test Recipe1','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+        new Recipe('Tasty Schnitzel','a super tasty Schnitzel -just awesome','https://therecipecritic.com/wp-content/uploads/2020/10/pork-schnitzel-recipe-3.jpg',
+        [new Ingredient('Meat',1),new Ingredient('french fries',20)]
+        )
       ,
-        new Recipe('A test Recipe2','this is simply the test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+        new Recipe('Big fat burger','what else you need to say?','https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg?w=2000',
+        [new Ingredient('Buns',2),new Ingredient('Meat',1)])
       
       ]
       getRecipes(){
