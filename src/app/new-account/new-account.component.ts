4d19edbacc009a821a45514b1bc96751af08a362
ihor97 <daniel.waffner@gmail.com>
2023-05-30 18:54:57 +0100

    109. Creating a Data Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,27 +1,27 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
   // тут вказується як ангулару створити інстанс сервіса
-  providers:[LoggingService]
+  providers:[LoggingService,AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 // ангулар створює сам інстанс намне треба створювати вручну
 // альтернативний варіант створення інстансу
 private loggingService?: LoggingService;
+private accountsService?:AccountsService;
   constructor() { 
     this.loggingService=inject(LoggingService)
+    this.accountsService=inject(AccountsService)
   }
-
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.loggingService.logStatusChange(accountStatus)
+// поки що нічого працювати не буде
+  onCreateAccount(name: string, status: string) {
+    this.accountsService.addAccount(name,status)
+    
+    this.loggingService.logStatusChange(status)
   }
 }
