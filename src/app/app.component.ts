30f5579871b71a5a7494997c6ff661a4276c88f8
ihor97 <daniel.waffner@gmail.com>
2023-06-01 20:25:23 +0100

    190. TD: Accessing the Form with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,19 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // другий варіант використовуючи ViewChild
+  @ViewChild('f') signupForm:NgForm
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   // f:NgForm тип автоматично створеної форми ангуларом
-  onSubmit(f:NgForm){
-    console.log(f);
+  // onSubmit(f:NgForm){
+  //   console.log(f);
+    
+  // }
+  onSubmit(){
+    console.log(this.signupForm);
     
   }
 
