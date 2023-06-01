0ae83a498f946c83d20c2ce5514468ee89363995
ihor97 <daniel.waffner@gmail.com>
2023-06-01 18:51:54 +0100

    178. Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+// subject і @Output є несумісними
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy{
   title = 'udemy-github';
+  activatedSub:Subscription
+  userActivated:boolean=false
+  constructor(private userService:UserService){
+
+  }
+ngOnInit(): void {
+  this.activatedSub=this.userService.activatedEmitter.subscribe(
+    (par)=>{
+      this.userActivated=par
+    }
+  )
+}
+ngOnDestroy(): void {
+  // від Subject треба обовязково відписуватися коли ми вручну юзаємо метод next
+  this.activatedSub.unsubscribe()
+}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,private userService:UserService) {
   }
 
   ngOnInit() {
@@ -17,5 +18,10 @@ export class UserComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
     });
+  }
+  onActivate(){
+    // у Subject є метод next()
+    // Subject є більш активним
+    this.userService.activatedEmitter.next(true)
   }
 }
