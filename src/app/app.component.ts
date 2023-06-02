cb6ac766a5e849622747d50cc667216e0fab7c67
ihor97 <daniel.waffner@gmail.com>
2023-06-02 14:01:35 +0100

    Assignment-TD my variant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,58 +7,12 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // другий варіант використовуючи ViewChild
-  @ViewChild('f') signupForm: NgForm
-  defaultQuestion = 'pet'
-  answer
-  genders = ['male', 'female']
-  user={
-    username:'',
-    email:'',
-    secretQuestion:'',
-    answer:'',
-    gender:''
+default='Advanced'
+@ViewChild('f') form:NgForm
+
+
+  submitForm(){
+    console.table(this.form.value);
+    
   }
-  submitted=false
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // через метод setValue можна задавати значення форми
-    // this.signupForm.setValue({
-    //   userData:{
-    //     username:suggestedName,
-    //     email:''
-    //   },
-    //   secret:'pet',
-    //   questionAnswer:'',
-    //   gender:'male'
-    // })
-    // цей підхід не переписує всю форму а змінює тільки те шо ми хочемо
-    this.signupForm.form.patchValue(
-      {
-        userData: {
-          username: suggestedName
-        }
-      }
-    )
-  }
-
-  // f:NgForm тип автоматично створеної форми ангуларом
-  // onSubmit(f:NgForm){
-  //   console.log(f);
-
-  // }
-  onSubmit() {
-    console.log(this.signupForm);
-    this.submitted=true
-    this.user.username=this.signupForm.value.userData.username
-    this.user.email=this.signupForm.value.userData.email
-    this.user.secretQuestion=this.signupForm.value.secret
-    this.user.answer=this.signupForm.value.questionAnswer
-    this.user.gender=this.signupForm.value.gender
-
-    // ресетаємо форму
-    this.signupForm.reset()
-
-  }
-
 }
