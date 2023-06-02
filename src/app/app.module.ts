217a0de0ceca23068ed56a9409492299b6cfee36
ihor97 <daniel.waffner@gmail.com>
2023-06-02 14:32:12 +0100

    203. Reactive: Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+// для реактивного підходу треба інший модуль 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
