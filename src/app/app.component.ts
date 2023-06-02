8e2e3593da173b380fa6ca41147e6f78d024df99
ihor97 <daniel.waffner@gmail.com>
2023-06-02 17:53:05 +0100

    211. Reactive: Arrays of Form Controls (FormArray)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +8,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-// тип реактивної форми
-  signupForm:FormGroup
-ngOnInit(): void {
-  this.signupForm=new FormGroup({
-    // створюмо групу
-    'userData':new FormGroup({
-      // добавляємо валідатори
-    'username':new FormControl(null, Validators.required),
-    // масив валідаторів
-    'email':new FormControl(null,[Validators.required,Validators.email]),
-    // тут ми вказуємо значення інпута
-    }),
+  // тип реактивної форми
+  signupForm: FormGroup
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      // створюмо групу
+      'userData': new FormGroup({
+        // добавляємо валідатори
+        'username': new FormControl(null, Validators.required),
+        // масив валідаторів
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        // тут ми вказуємо значення інпута
+      }),
 
-    'gender':new FormControl('male')
-  })
-}
-onSubmit(){
-  // так як ми створили форму ми зразу можемо отримати доступ до неї
-  console.log(this.signupForm);
-  
-}
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+  }
+  onAddHobbys() {
+    const control=new FormControl(null,Validators.required);
+    // кастуємо
+    (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+  onSubmit() {
+    // так як ми створили форму ми зразу можемо отримати доступ до неї
+    console.log(this.signupForm);
+
+  }
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
 }
