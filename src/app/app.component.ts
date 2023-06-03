b2414397968edfefe93b0e645edc697570b143ec
ihor97 <daniel.waffner@gmail.com>
2023-06-03 10:52:51 +0100

    Reactive Forms Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  selectStatus=['Stable', 'Critical', 'Finished']
+  forbidden='Test'
+  signupForm:FormGroup
+
+  ngOnInit(): void {
+    this.signupForm=new FormGroup({
+      data:new FormGroup({
+        project:new FormControl(null,[Validators.required],this.asyncValidator),
+        email:new FormControl(null,[Validators.email,Validators.required])
+      }),
+      status:new FormControl(null)
+    })
+    
+  }
+  onSubmit(){
+    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+    
+    
+  }
+  checkForbiddenName(control:FormControl):{[s:string]:boolean}{
+    if(control.value==this.forbidden){
+      return {'isForbidden':true}
+    }
+    return null
+  }
+
+  asyncValidator(control:FormControl):Promise<any>|Observable<any>{
+
+    return new Promise((res,rej)=>{
+      setTimeout(() => {
+        if (control.value==='Test') {
+          res({'isForbidden':true})
+        }
+        res(null)
+      }, 2000);
+    })
+  }
+
 }
