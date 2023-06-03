137ee01aad4fcc78c64ed14f5158c96f5382a9f1
ihor97 <daniel.waffner@gmail.com>
2023-06-03 13:35:00 +0100

    228. Creating the Form For Editing Recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { SelectRecipeComponent } from './recipes/select-recipe/select-recipe.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,12 +29,13 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     DropdownDirective,
     SelectRecipeComponent,
     RecipeEditComponent,
-    
+   
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule 
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,23 +9,51 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  id:number
-  editMode=false
-  constructor(private actRoute:ActivatedRoute) { }
+  recipeForm: FormGroup
+  id: number
+  editMode = false
+  constructor(private actRoute: ActivatedRoute,
+    private recipeService: RecipesService
+  ) { }
 
   ngOnInit(): void {
     this.actRoute.params.subscribe(
-      (params:Params)=>{
-        if(params['id']){
-          this.id=+params['id']
-          this.editMode=true
-        }else{
+      (params: Params) => {
+        if (params['id']) {
+          this.id = +params['id']
+          this.editMode = true
+        } else {
           console.log(+params['id']);
-          
-          this.editMode=false
+          // викликаємо метод initForm тут тому що наша урла буде мінятися
+          this.initForm()
+          this.editMode = false
+
         }
       }
     )
-  }
 
+
+  }
+  onSubmit() {
+    console.log(this.recipeForm);
+
+  }
+  private initForm() {
+    let recipeName = ''
+    let recipeImagePath = ''
+    let recipeDescription = ''
+    if (this.editMode) {
+      const recipe = this.recipeService.getOneRecipe(this.id)
+      recipeName = recipe.name
+      recipeImagePath = recipe.imagePath
+      recipeDescription = recipe.description
+      this.recipeForm = new FormGroup({
+        name: new FormControl(recipeName),
+        imagePath: new FormControl(recipeImagePath),
+        description: new FormControl(recipeDescription),
+
+      })
+    }
+
+  }
 }
