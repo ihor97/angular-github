01c847a0323a5a68dab54e5d280e81571dd778c5
ihor97 <daniel.waffner@gmail.com>
2023-06-04 14:39:17 +0100

    262. Using a Service for Http Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {  map} from "rxjs/operators";
 import { Post } from './post.model';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
@@ -12,59 +13,27 @@ export class AppComponent implements OnInit {
   loadedPosts:Post[] = [];
   isFetching=false
   // інжектимо клієнт 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private postService:PostService) { }
 
   ngOnInit() {
-    this.fetchPosts()
+    this.postService.fetchPosts()
+
   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
-    this.http.post<{[key:string]:Post}>(
-      'https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json',
-      postData
-    ).subscribe(
-      res => {
-        console.log(res);
-      }
-    )
-
+    this.postService.createAndStorePost(postData.title,postData.content)
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts()
+    this.postService.fetchPosts()
 
   }
 
   onClearPosts() {
     // Send Http request
   }
-  private fetchPosts() {
-    this.isFetching=true
-    this.http
-    // кращий варіант через дженерік
-      .get<{[key:string]:Post}>('https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json')
-      // трансформуємо наші дані які бужемо отримувати
-      .pipe(map(
-        // робимо тип для даних які ми приймаємо
-        (response)=>{
-          const postsArray:Post[]=[]
-          for (const key in response) {
-            if(response.hasOwnProperty(key)){
-              // додаємо ще  айді до нашого обєкту з даними
-              postsArray.push({...response[key],id:key})
-            }
-          }
-          return postsArray
-        }
-      ))
-      .subscribe(
-        posts => {
-          this.loadedPosts=posts
-          this.isFetching=false
-        }
-      )
-  }
+ 
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import {  map} from "rxjs/operators";
+
+
+@Injectable(
+    { providedIn: 'root'}
+)
+export class PostService {
+    
+    constructor(private http:HttpClient){}
+    createAndStorePost(title:string,content:string){
+        const postData:Post={title,content}
+        this.http.post<{[key:string]:Post}>(
+            'https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json',
+            postData
+          ).subscribe(
+            res => {
+              console.log(res);
+            }
+          )
+    }
+    fetchPosts(){
+        this.http
+        // кращий варіант через дженерік
+          .get<{[key:string]:Post}>('https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json')
+          // трансформуємо наші дані які бужемо отримувати
+          .pipe(map(
+            // робимо тип для даних які ми приймаємо
+            (response)=>{
+              const postsArray:Post[]=[]
+              for (const key in response) {
+                if(response.hasOwnProperty(key)){
+                  // додаємо ще  айді до нашого обєкту з даними
+                  postsArray.push({...response[key],id:key})
+                }
+              }
+              return postsArray
+            }
+          ))
+          .subscribe(
+            posts => {
+                
+            }
+          )
+    }
+
+}
