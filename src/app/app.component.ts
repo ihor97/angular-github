2387b27de119dfef0e204c9f4de5fa14f94a049c
ihor97 <daniel.waffner@gmail.com>
2023-06-04 12:12:31 +0100

    257. GETting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,39 @@ export class AppComponent implements OnInit {
 // інжектимо клієнт 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
-    // posts.json треба обовязково додавати коли юзаємо файрбейз так вимагаї її API
     this.http.post(
       'https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json',
-      // ангулар бере наш обєкт і автоматично його ковертує в json
     postData
-    // якщо ми не підписуємся ангулар не буде відправляти запит так як дума що ніхто не зацікавлений в поверненні даних
     ).subscribe(
       res=>{
         console.log(res);
-        
       }
     )
 
-    // метод post вертає Observable 
-    // браузер робить два запроса перший з options - вказує чи можна нам робити запрос, а другий - уже наш запрос
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
+
   }
 
   onClearPosts() {
     // Send Http request
+  }
+  private fetchPosts(){
+    this.http.get('https://ng-udemy-80a0b-default-rtdb.firebaseio.com/posts.json').subscribe(
+      posts=>{
+        console.log(posts);
+        
+      }
+    )
   }
 }
