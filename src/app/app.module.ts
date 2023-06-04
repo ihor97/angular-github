3b1626adeb8d3d1bdc4ddd2927d73ad4534e866a
ihor97 <daniel.waffner@gmail.com>
2023-06-04 18:27:58 +0100

    276. Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth.-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   // провайдимо інтерцептор
   // HTTP_INTERCEPTORS токен
-  providers: [{ 
+  // є важливий порядок інтерцепторів
+  providers: [{
     provide: HTTP_INTERCEPTORS,
-     useClass: AuthInterceptorService, 
-     multi: true }],
+    useClass: AuthInterceptorService,
+    multi: true
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: LoggingInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth.-interceptor.service.ts
+++ src/app/auth.-interceptor.service.ts
@@ -6,25 +6,11 @@ export class AuthInterceptorService implements HttpInterceptor{
     // запускається перед тим як йде запит
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // if(req.url) якщо ми не хочмо робити запит по якісь урлі ми можемо його перервати
-        console.log('Request on its way');
-        console.log(req.url);
-        
         // продовжуємо наш запит
         // реквест не можна модифікувати  якщо ми хочемо модифікувати то треба робити новий
         const modifiedRequest=req.clone({headers:req.headers.append('Auth','xyz')})
 
         // можна також модифікувати відповідь
-        return next.handle(modifiedRequest).pipe(
-            tap(event=>{
-                console.log(event);
-                
-                if(event.type===HttpEventType.Response){
-                    console.log('response arrived');
-                    console.log(event.body);
-                    
-                    
-                }
-            })
-        )
+        return next.handle(modifiedRequest)
     }
 }
