4806ec79532f9fb232cb771f5b363f266424a3ed
ihor97 <daniel.waffner@gmail.com>
2023-06-05 16:31:55 +0100

    20-294. Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+//формат відповіді з сервера 
+interface AuthResponseData {
+    kind:string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+    constructor(private http: HttpClient) { }
+    signUp(email: string, password: string) {
+        // так як нам треба відповідь з сервера ми повертаємо цей Observable
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyBxIDbEeDSzxrzQcbcg8dDM2rTB8PIKcxA]',
+            {
+                email,
+                password,
+                returnSecureToken: true
+            })
+    }
+}
