873b0251a3430fd83f3785dc0bfd095200df698a
ihor97 <daniel.waffner@gmail.com>
2023-06-05 18:50:25 +0100

    20-299. Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 // rxjs обгортає помилку в Observable
@@ -27,28 +27,40 @@ export class AuthService {
                 password,
                 returnSecureToken: true
             }).pipe(
-                catchError(errorRes => {
-                    let errorMessage = 'An unknown error occured'
-                    if (!errorRes.error || !errorRes.error.error) {
-                        // так як не всі помилки мають властивість error або error.error
-                        // тому невідомі помилки ми викидуємо через метод throwError з rxjs 
-                        return throwError(errorMessage)
-                    }
-                    switch (errorRes.error.error.message) {
-                        case 'EMAIL_EXISTS':
-                            errorMessage = 'The email address is already in use by another account'
-                    }
-                    return throwError(errorMessage)
-                })
+                // ми можемо просто передати силку на ф-ю і параметр закинеться туди автоматично
+                catchError(this.handleError)
             )
     }
 
     login(email: string, password: string) {
-       return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxIDbEeDSzxrzQcbcg8dDM2rTB8PIKcxA',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxIDbEeDSzxrzQcbcg8dDM2rTB8PIKcxA',
             {
                 email,
                 password,
                 returnSecureToken: true
-            })
+            }).pipe(
+                catchError(this.handleError)
+            )
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured'
+        if (!errorRes.error || !errorRes.error.error) {
+            // так як не всі помилки мають властивість error або error.error
+            // тому невідомі помилки ми викидуємо через метод throwError з rxjs 
+            return throwError(errorMessage)
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account'
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage='There is no user record corresponding to this identifier. The user may have been deleted.'
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage='The password is invalid or the user does not have a password.'
+                break;
+        }
+        return throwError(errorMessage)
     }
 }
