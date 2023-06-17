a9e4aaf0350d36a5a02eb78bf933138a79503f90
ihor97 <daniel.waffner@gmail.com>
2023-06-17 21:27:05 +0100

    351. A Standalone Root Component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
+// для того щоб зробити рут компоненту стандалоне треба бутстрапити з компоненти а не з модуля
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent)
