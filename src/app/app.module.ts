74a8291149ad15896a0cc94c4299c443cd10f47b
ihor97 <daniel.waffner@gmail.com>
2023-06-17 21:15:46 +0100

    350. Migrating Another Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { DetailsComponent } from './welcome/details/details.component';
 
 // коли ми прописуємо standalone тоді з модуля треба видалити import
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
+  declarations: [AppComponent],
   // сюди закидуємо standalone components
-  imports: [BrowserModule,DetailsComponent],
+  imports: [BrowserModule,WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-// import { DetailsComponent } from './details/details.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
+  standalone:true,
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   // можна буде юзати тоді коли наша компента буде standalone 
-  // imports:[DetailsComponent]
+  imports:[DetailsComponent]
 })
 export class WelcomeComponent {}
